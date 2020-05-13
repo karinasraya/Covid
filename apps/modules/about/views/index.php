9a578b3d17bc357efc32e9eb10ed59ca7e696f4b
ayumutiarasari98 <56009915+ayumutiarasari98@users.noreply.github.com>
2020-05-13 19:21:46 +0700

    ubah view about

--- apps/modules/about/views/index.php
+++ apps/modules/about/views/index.php
@@ -1,40 +1,311 @@
-<div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-        <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
-            <div class="row flex-grow">
-                <div class="col-lg-6 d-flex align-items-center justify-content-center">
-                    <div class="auth-form-transparent text-left p-3">
-                        <div class="brand-logo">
-                            <img src="<?= base_url('kapella/images/logo.jpg'); ?>" alt="logo">
-                        </div>
-                        <h4>Pudyasto Adi Wibowo</h4>
-                        <h6 class="font-weight-light">Web Developer</h6>
-                        <p class="font-weight-light">
-                            Always try to challenges and learning about new things.
-                            <br>
-                            "Stop learning, start dying"
-                        </p>
-                        <form class="pt-3">
-                            <div class="mb-2 d-flex">
-                                <a href="https://www.facebook.com/dhyaz.cs" class="btn btn-facebook auth-form-btn flex-grow mr-1">
-                                    <i class="mdi mdi-facebook mr-2"></i>Facebook
-                                </a>
-                                <a href="mailto:pawdev.id@gmail.com?Subject=Support INA Covid-19" class="btn btn-google auth-form-btn flex-grow ml-1">
-                                    <i class="mdi mdi-google mr-2"></i>Google
-                                </a>
-                            </div>
-                        </form>
-                        <a href="<?=site_url('home');?>" class="">
-                            Back to Home
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-6 login-half-bg d-flex flex-row">
-                    <p class="text-white font-weight-medium text-center flex-grow align-self-end">Copyright &copy; 2018 All rights reserved.</p>
-                </div>
-            </div>
-        </div>
-        <!-- content-wrapper ends -->
-    </div>
-    <!-- page-body-wrapper ends -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+}
+
+.container {
+  padding: 0 16px;
+  text-align: center;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: black;
+  text-align: center;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+
+@media screen and (max-width: 650px) {
+  .column, .columncon, input[type=submit] {
+    width: 100%;
+    display: block;
+  }
+}
+</style>
+</head>
+<body>
+
+<div class="about-section">
+<h1>Kontak Layanan untuk COVID-19</h1>
 </div>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>COVID-19 Hotline</h2> 
+            <p class="mdi mdi-phone"> 119</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Layanan Pengaduan Program Keluarga Harapan Kementerian Sosial</h3> 
+            <p class="mdi mdi-phone"> 1500299 </p>
+            <p> Setiap hari kerja 08.00 s.d. 17.00 WIB </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Layanan Pengaduan Bansos Kementerian Sosial</h3> 
+            <p class="mdi mdi-phone"> 157 </p>
+            <p class="mdi mdi-phone"> 0811-10-222-10(WA) </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Layanan Masyarakat Kartu Prakerja</h3> 
+            <p class="mdi mdi-phone"> 021-25541246 </p>
+            <p> Setiap hari kerja 08.00 s.d. 19.00 WIB </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Bantuan Langsung Tunai Desa</h3> 
+            <p class="mdi mdi-phone"> 1500040 </p>
+            <p class="mdi mdi-message-text"> 0877-8899-0040 atau 0812-8899-0040 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Pembebasan Biaya Listrik (PLN)</h3> 
+            <p class="mdi mdi-phone"> 123 </p>
+            <p class="mdi mdi-phone"> 08122-123-123 </p>
+      </div>
+    </div>
+  </div>
+  
+  
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Tambahan insentif perumahan Kementerian PUPR</h3> 
+            <p class="mdi mdi-email"> informasi@pu.go.id </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Informasi lebih lanjut mengenai perpajakan</h3> 
+            <p class="mdi mdi-email"> informasi@pajak.go.id</p>
+            <p class="mdi mdi-phone"> 1500200 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Informasi lebih lanjut mengenai bea masuk dan cukai</h3> 
+            <p class="mdi mdi-email"> info@customs.go.id</p>
+            <p class="mdi mdi-phone"> 1500225 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h4>Informasi lebih lanjut mengenai Restrukturisasi Kredit atau Pembiayaan terkait dampak COVID-19 Layanan Kontak OJK</h4> 
+            <p class="mdi mdi-phone"> 157 </p>
+            <p class="mdi mdi-phone"> 081-157-157-157 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h4>Posko Pengaduan Daring bagi Masyarakat Terdampak Bencana Nasional COVID-19
+Ombudsman Pusat</h4> 
+            <p class="mdi mdi-email"> covid19-pusat@ombudsman.go.id </p>
+            <p class="mdi mdi-phone"> 0811-919-3737 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Kejahatan Narkotika BNN</h3> 
+            <p class="mdi mdi-phone"> 184 </p>
+            <p class="mdi mdi-phone"> 0812-2167-5675 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Pelayanan SEJIWA Kemen PPPA</h3> 
+            <p class="mdi mdi-email"> pengaduan@kemenpppa.go.id </p>
+            <p class="mdi mdi-phone"> 119 ext. 8 / 0821-2575-1234 / 0811-1922-911 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>WA Center Kementerian Agama Sigap COVID-19</h3> 
+        <p class="mdi mdi-phone"> 081-1159-9003 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Informasi Fungsi Relawan Desa Lawan COVID-19 Kementerian Desa, PDT, dan Transmigrasi</h3> 
+            <p class="mdi mdi-phone"> 1500040 </p>
+            <p class="mdi mdi-message-text"> 0877-8899-0040 atau 0812-8899-0040 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Jalur Pengaduan bagi Pelaku Pariwisata dan Ekonomi Kreatif Terdampak COVID-19</h3> 
+            <p class="mdi mdi-email"> info@kemenparekraf.go.id </p>
+            <p class="mdi mdi-phone"> 0811-8956-767 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h3>Layanan Pengiriman Surat ke Kementerian Perdagangan</h3> 
+            <p class="mdi mdi-email"> persuratan@kemendag.go.id </p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+
+
+<div class="about-section">
+  <h1>About Us</h1>
+  <p>Website ini merupakan project yang digunakan untuk memenuhi Final Project kelas PBKK A</p>
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Karina Soraya Puspita Sari</h2>
+        <p class="title">0511174000003</p>
+        <p>Karinasoraya.ks@gmail.com</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Celia Chintara Yuwine</h2>
+        <p class="title">05111740000058</p>
+        <p>celiachintara@ymail.com</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Ayu Mutiara Sari</h2>
+        <p class="title">05111740000149</p>
+        <p>ayumtrsr@gmail.com</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
